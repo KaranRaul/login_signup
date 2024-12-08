f3a75496d2953075b0d887ec39cbb699ce4f26ac
KaranRaul <karanraul02@gmail.com>
2024-12-08 22:14:57 +0530

    render api url added

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,5 +1,5 @@
 export const login = async (email: string, password: string) => {
-    const response = await fetch('http://localhost:5000/api/auth/login', {
+    const response = await fetch('https://login-signup-3cz3.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
@@ -7,7 +7,7 @@ export const login = async (email: string, password: string) => {
     return response.json();
 };
 export const register = async (name: string, email: string, password: string) => {
-    const response = await fetch('http://localhost:5000/api/auth/register', {
+    const response = await fetch('https://login-signup-3cz3.onrender.com/api/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name, email, password }),
